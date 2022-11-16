284fe5acb5619ec09421e80cc77513a74f1c6aec
DmitriiVolkovIzh <booyakaa@mail.ru>
2022-11-16 10:20:05 +0400

    ДЗ 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Human maksim = new Human(1988,"Максим","Минск","бренд-менеджер");
+        maksim.introduceYourself();
+
+        Human anna = new Human(1993,"Аня","Москва","методист образовательных программ");
+        anna.introduceYourself();
+
+        Human kate = new Human(1992,"Катя","Калининград","продакт-менеджер");
+        kate.introduceYourself();
+
+        Human artem = new Human(1995,"Артём","Москва","директор по развитию бизнеса");
+        artem.introduceYourself();
     }
 }
