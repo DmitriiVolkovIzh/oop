422befb049bf01bcc222b2574e44202aa0eaa455
DmitriiVolkovIzh <booyakaa@mail.ru>
2022-11-17 08:58:47 +0400

    ДЗ 1 с дополнениями

--- src/Human.java
+++ src/Human.java
@@ -4,15 +4,23 @@ public class Human {
     String town;
     String jobTitle;
 
+    String noInfo = " <информация не указана>";
 
-    void introduceYourself() {
-        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году.Я работаю на должности " + jobTitle + ".  Будем знакомы!");
+
+    public String toString(){
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году.Я работаю на должности " + jobTitle + ".  Будем знакомы!";
     }
 
     public Human(int yearOfBirth, String name, String town, String jobTitle) {
-        this.yearOfBirth = yearOfBirth;
-        this.name = name;
-        this.town = town;
-        this.jobTitle = jobTitle;
+        if (yearOfBirth >= 0)
+            this.yearOfBirth = yearOfBirth;
+        else this.yearOfBirth = Math.abs(yearOfBirth);
+        if(name == null || name.isEmpty()) this.name = noInfo;
+        else this.name = name;
+        if(town == null || town.isEmpty()) this.town = noInfo;
+        else this.town = town;
+        if(jobTitle == null || jobTitle.isEmpty()) this.jobTitle = noInfo;
+        else this.jobTitle = jobTitle;
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human(1988,"Максим","Минск","бренд-менеджер");
-        maksim.introduceYourself();
+        Human maksim = new Human(1988,"","Минск","бренд-менеджер");
+        System.out.println(maksim);
 
-        Human anna = new Human(1993,"Аня","Москва","методист образовательных программ");
-        anna.introduceYourself();
+        Human anna = new Human(-1993,"Аня","Москва","методист образовательных программ");
+        System.out.println(anna);
+
 
         Human kate = new Human(1992,"Катя","Калининград","продакт-менеджер");
-        kate.introduceYourself();
+        System.out.println(kate);
+
 
         Human artem = new Human(1995,"Артём","Москва","директор по развитию бизнеса");
-        artem.introduceYourself();
+        System.out.println(artem);
+
     }
 }
